ddd1d0ef42a3ca07c5aeb3e3d849eabdbd55a0c1
jcarbolynn <36271436+jcarbolynn@users.noreply.github.com>
2018-02-14 14:27:12 -0500

    Add files via upload

---
+++ AsciiArt.java
@@ -0,0 +1,17 @@
+public class AsciiArt {
+   public static void main(String[] args) {
+
+      // Draw tree
+      System.out.println("   *");
+      System.out.println("  ***");
+      System.out.println(" *****");
+      System.out.println("*******");
+      System.out.println("  ***");
+      
+      System.out.println("\n" + "\n" + "/\\   /\\");
+      System.out.println("  o o");
+      System.out.println(" =   =");
+      System.out.println("  ---");
+      
+   }
+}
